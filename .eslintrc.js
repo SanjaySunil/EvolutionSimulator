eecd9421175e4c751afc0ed3e1ca7308ad00149b
Sanjay Sunil <sanjaysunil@protonmail.com>
2023-07-14 20:09:23 +0100

    chore: add configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,15 @@ module.exports = {
   'env': {
     'browser': true,
     'commonjs': true,
-    'es2021': true,
+    'es2023': true,
   },
   'extends': 'google',
   'parserOptions': {
     'ecmaVersion': 'latest',
+    'sourceType': 'module',
   },
   'rules': {
+    'camelcase': 0,
+    'require-jsdoc': 0,
   },
 };
