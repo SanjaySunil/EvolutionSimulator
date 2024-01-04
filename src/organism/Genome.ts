5d8bf2e575485567aed2a8f33319cd5f4b67b99d
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-01-04 21:05:44 +0000

    fix comments

--- src/organism/Genome.ts
+++ src/organism/Genome.ts
@@ -26,23 +26,23 @@ export default class Genome {
       if (this.data) {
         let genomeString = "";
 
-        /** Convert each individual gene into a genome string. */
+        // Convert each individual gene into a genome string.
         for (const gene of this.data) {
           genomeString += `${gene.source_type},${gene.source_id},${gene.sink_type},${gene.sink_id},${gene.weight}`;
         }
 
-        /** Create a unique hash from the genome string. */
+        // Create a unique hash from the genome string.
         let hash = 0;
         for (let i = 0; i < genomeString.length; i++) {
           hash = (hash << 5) - hash + genomeString.charCodeAt(i);
         }
 
-        /** Calculate the rgb codes using the hash. */
+        // Calculate the rgb codes using the hash.
         let r = (hash & 0xff0000) >> 16;
         let g = (hash & 0x00ff00) >> 8;
         let b = hash & 0x0000ff;
 
-        /** Normalize RGB values to fit within the range of 0-255. */
+        // Normalize RGB values to fit within the range of 0-255.
         r = Math.floor(r % 256);
         g = Math.floor(g % 256);
         b = Math.floor(b % 256);
