48d1366ef0febe553a32a1948b03708467beb77f
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-01-04 21:08:43 +0000

    fix code

--- src/organism/Genome.ts
+++ src/organism/Genome.ts
@@ -11,45 +11,35 @@ export default class Genome {
   constructor(owner: Organism, genome: Gene[]) {
     this.owner = owner;
     this.data = cloneDeep(genome);
-    this.colour = this.get_colour().then(() => this.get_colour());
+    this.colour = this.get_colour();
   }
 
-  public copy_genome(genome): Promise<string> {
-    return new Promise((resolve) => {
-      this.data = cloneDeep(genome);
-      resolve("Genome copied.");
-    });
-  }
+  public get_colour(): string {
+    if (this.data) {
+      let genomeString = "";
 
-  public get_colour(): any {
-    return new Promise((resolve) => {
-      if (this.data) {
-        let genomeString = "";
-
-        // Convert each individual gene into a genome string.
-        for (const gene of this.data) {
-          genomeString += `${gene.source_type},${gene.source_id},${gene.sink_type},${gene.sink_id},${gene.weight}`;
-        }
-
-        // Create a unique hash from the genome string.
-        let hash = 0;
-        for (let i = 0; i < genomeString.length; i++) {
-          hash = (hash << 5) - hash + genomeString.charCodeAt(i);
-        }
-
-        // Calculate the rgb codes using the hash.
-        let r = (hash & 0xff0000) >> 16;
-        let g = (hash & 0x00ff00) >> 8;
-        let b = hash & 0x0000ff;
-
-        // Normalize RGB values to fit within the range of 0-255.
-        r = Math.floor(r % 256);
-        g = Math.floor(g % 256);
-        b = Math.floor(b % 256);
-
-        this.colour = `rgb(${r}, ${g}, ${b})`;
-        resolve("Organism colour calculated.");
+      // Convert each individual gene into a genome string.
+      for (const gene of this.data) {
+        genomeString += `${gene.source_type},${gene.source_id},${gene.sink_type},${gene.sink_id},${gene.weight}`;
       }
-    });
+
+      // Create a unique hash from the genome string.
+      let hash = 0;
+      for (let i = 0; i < genomeString.length; i++) {
+        hash = (hash << 5) - hash + genomeString.charCodeAt(i);
+      }
+
+      // Calculate the rgb codes using the hash.
+      let r = (hash & 0xff0000) >> 16;
+      let g = (hash & 0x00ff00) >> 8;
+      let b = hash & 0x0000ff;
+
+      // Normalize RGB values to fit within the range of 0-255.
+      r = Math.floor(r % 256);
+      g = Math.floor(g % 256);
+      b = Math.floor(b % 256);
+
+      return `rgb(${r}, ${g}, ${b})`;
+    } else throw Error("Genome has no data.");
   }
 }
