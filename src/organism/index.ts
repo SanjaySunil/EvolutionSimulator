264f06c9f635464406bf2218813a14473d852857
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-01-05 11:20:17 +0000

    add comments

--- src/organism/index.ts
+++ src/organism/index.ts
@@ -9,6 +9,7 @@ import Genome from "./Genome";
 import { ActionNeurons } from "./Neurons";
 import { SimulationConfig } from "../config";
 
+// Represents an organism in the simulation.
 export default class Organism {
   private _coordinate: Coordinate;
   public genome: Genome;
@@ -22,10 +23,11 @@ export default class Organism {
 
   public config: typeof SimulationConfig;
 
+  // Constructs a new Organism object.
   constructor(coordinate, genome: Gene[], environment) {
     this.environment = environment;
     this._coordinate = coordinate;
-    /** If a genome has been given, use this genome, else create a new random genome. */
+    // If a genome has been given, use this genome, else create a new random genome.
     this.genome = new Genome(this, genome);
     this.config = environment.config;
     this.brain = new Brain(
@@ -40,16 +42,19 @@ export default class Organism {
     this.age = 0;
   }
 
+  // Sets the coordinate of the organism.
   public set coordinate(coordinate: Coordinate) {
     this.environment.grid.clear_cell_state(this.coordinate);
     this.environment.grid.set_cell_owner(coordinate, this);
     this._coordinate = coordinate;
   }
 
+  // Gets the coordinate of the organism.
   public get coordinate(): Coordinate {
     return this._coordinate;
   }
 
+  // Mates with another organism to produce a child organism.
   public mate(partner: Organism): Organism {
     const child_genome: Gene[] = new Array(this.genome.data.length);
 
@@ -77,15 +82,17 @@ export default class Organism {
     return new Organism(random_coord, child_genome, this.environment);
   }
 
+  // Performs an action based on the organism's brain.
   public action(): Coordinate {
-    /** Perform NN Feed Forward. */
+    // Perform NN Feed Forward.
     const action_levels = this.brain.feed_forward();
-    /** Compute function with action levels. */
+    // Compute function with action levels.
     return this.compute_action(action_levels);
   }
 
+  // Computes the action based on the action levels.
   public compute_action(action_levels: number[]): Coordinate {
-    /** Move x and Move y is the urge to move in the X and Y direction. */
+    // Move x and Move y is the urge to move in the X and Y direction.
     let move_x = action_levels[ActionNeurons.MOVE_X];
     let move_y = action_levels[ActionNeurons.MOVE_Y];
 
@@ -94,7 +101,7 @@ export default class Organism {
     move_y += action_levels[ActionNeurons.MOVE_NORTH];
     move_y -= action_levels[ActionNeurons.MOVE_SOUTH];
 
-    /** Hyperbolic tangent function produces an output between -1.0 and 1.0. */
+    // Hyperbolic tangent function produces an output between -1.0 and 1.0.
     move_x = Math.tanh(move_x);
     move_y = Math.tanh(move_y);
 
