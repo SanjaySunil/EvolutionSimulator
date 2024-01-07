9b4d96770fff6df0fd008466b39676848077d813
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-01-07 18:36:49 +0000

    removed mutate gene

--- evolution_simulator/client/src/models/Gene.ts
+++ evolution_simulator/client/src/models/Gene.ts
@@ -21,22 +21,4 @@ export default class Gene {
     this.sink_id = get_random_number(0, 0x7fff) % NUMBER_OF_NEURONS;
     this.weight = Math.round(Math.random() * 0xffff) - 0x8000;
   }
-
-  // Mutates the gene at index.
-  public mutate_gene(): void {
-    const bitMask = 1 << get_random_number(0, 15);
-    const chance = Math.random();
-
-    if (chance < 0.2) {
-      this.source_type ^= 1;
-    } else if (chance < 0.4) {
-      this.sink_type ^= 1;
-    } else if (chance < 0.6) {
-      this.source_id ^= bitMask;
-    } else if (chance < 0.8) {
-      this.sink_id ^= bitMask;
-    } else {
-      this.weight ^= bitMask;
-    }
-  }
 }
