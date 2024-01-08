8ac44cacdf320f7efc86f07786551ae2c33d62eb
Sanjay Sunil <sanjayysunil@gmail.com>
2024-01-08 18:27:42 +0000

    Update Organism.ts

--- evolution_simulator/client/src/models/Organism.ts
+++ evolution_simulator/client/src/models/Organism.ts
@@ -48,45 +48,55 @@ export default class Organism {
 
   // Sets the coordinate of the organism.
   public set coordinate(coordinate: Coordinate) {
+    // Clears the current cell state of the organism's previous coordinate.
     this.grid.clear_cell_state(this.coordinate);
+    // Sets the cell owner to the current organism at the new specified coordinate.
     this.grid.set_cell_owner(coordinate, this);
+    // Updates the organism's _coordinate property with the new coordinate value.
     this._coordinate = coordinate;
   }
+
 
   // Gets the coordinate of the organism.
   public get coordinate(): Coordinate {
     return this._coordinate;
   }
 
+
   // Performs an action based on the organism's brain.
   public action(): Coordinate {
-    // Perform NN Feed Forward.
+    // Perform Neural Network Feed Forward to obtain action levels.
     const action_levels = this.brain.feed_forward();
-    // Compute function with action levels.
+    // Compute the action to take based on the action levels.
     return this.compute_action(action_levels);
   }
 
+
   // Computes the action based on the action levels.
   public compute_action(action_levels: number[]): Coordinate {
-    // Move x and Move y is the urge to move in the X and Y direction.
+    // 'move_x' and 'move_y' represent the urge to move in the X and Y direction respectively.
     let move_x = action_levels[OutputNeurons.MOVE_X];
     let move_y = action_levels[OutputNeurons.MOVE_Y];
 
+    // Accumulate the urge to move based on specific output neurons representing directions.
     move_x += action_levels[OutputNeurons.MOVE_EAST];
     move_x -= action_levels[OutputNeurons.MOVE_WEST];
     move_y += action_levels[OutputNeurons.MOVE_NORTH];
     move_y -= action_levels[OutputNeurons.MOVE_SOUTH];
 
-    // Hyperbolic tangent function produces an output between -1.0 and 1.0.
+    // The hyperbolic tangent function confines movement values between -1.0 and 1.0.
     move_x = Math.tanh(move_x);
     move_y = Math.tanh(move_y);
 
+    // Calculate probabilities based on absolute values.
     const prob_x = +prob2bool(Math.abs(move_x));
     const prob_y = +prob2bool(Math.abs(move_y));
 
+    // Determine direction of movement based on the sign of the values.
     const signum_x = move_x < 0.0 ? -1 : 1;
     const signum_y = move_y < 0.0 ? -1 : 1;
 
+    // Create a vector representing the calculated movement based on probabilities and directions.
     const offset = make_vector(prob_x * signum_x, prob_y * signum_y);
     return offset;
   }
