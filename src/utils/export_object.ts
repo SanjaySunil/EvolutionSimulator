47d025b14b66ce075c1ae8bd629e625422a5ee2f
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-01-12 11:47:06 +0000

    add jsdoc

--- src/utils/export_object.ts
+++ src/utils/export_object.ts
@@ -1,3 +1,8 @@
+/**
+ * Export an object as a JSON file.
+ * @param object - The object to export.
+ * @param export_name - The name of the exported file.
+ */
 export default function export_object(object, export_name): void {
   const data_string = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(object));
   const download_anchor_node = document.createElement("a");
--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -1,16 +1,31 @@
 import { Coordinate } from "../types/Coordinate";
 
-// Converts an angle to radians.
+/**
+ * Converts an angle to radians.
+ * @param angle - The angle to convert to radians.
+ * @returns The angle in radians.
+ */
 export function to_radians(angle): number {
   return (Math.PI / 180) * angle;
 }
 
-// Calculates the Euclidean distance between two coordinates.
+/**
+ * Calculates the Euclidean distance between two coordinates.
+ * @param first_coord - The first coordinate.
+ * @param second_coord - The second coordinate.
+ * @returns The Euclidean distance between the two coordinates.
+ */
 export function euclidean_distance(first_coord: Coordinate, second_coord: Coordinate): number {
   return Math.sqrt(Math.pow(second_coord.y - first_coord.y, 2) + Math.pow(second_coord.x - first_coord.x, 2));
 }
 
-// Rotates a point about a pivot by a specified angle.
+/**
+ * Rotates a point about a pivot by a specified angle.
+ * @param point - The point to rotate.
+ * @param pivot - The pivot to rotate the point about.
+ * @param angle - The angle to rotate the point by.
+ * @returns The rotated point.
+ */
 export function rotate_point(point: Coordinate, pivot: Coordinate, angle: number): Coordinate {
   if (typeof point.x == "number" && typeof point.y == "number") {
     const radians = to_radians(angle);
@@ -24,17 +39,31 @@ export function rotate_point(point: Coordinate, pivot: Coordinate, angle: number
   }
 }
 
-// Performs addition of two vectors.
+/**
+ * Performs addition of two vectors.
+ * @param first_coord
+ * @param second_coord
+ * @returns The sum of the two vectors.
+ */
 export function add_vector(first_coord, second_coord): Coordinate {
   return { x: first_coord.x + second_coord.x, y: first_coord.y + second_coord.y };
 }
 
-// Creates a coordinate object, treating as a vector.
+/**
+ * Creates a coordinate object, treating as a vector.
+ * @param x - The x-coordinate of the vector.
+ * @param y - The y-coordinate of the vector.
+ * @returns The coordinate object.
+ */
 export const make_vector = (x: number, y: number): Coordinate => {
   return { x: x, y: y };
 };
 
-// Converts a point to an angle in degrees.
+/**
+ * Converts a point to an angle in degrees.
+ * @param point - The point to convert to an angle.
+ * @returns The angle in degrees.
+ */
 export function to_angle(point: Coordinate): number {
   const firstAngle = Math.atan2(1, 0);
   const secondAngle = Math.atan2(point.y, point.x);
