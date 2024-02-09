c705674b5ef82912e21a30c4612cb99e88e47043
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-02-09 11:22:47 +0000

    Revert "update new"
    This reverts commit 7305dde2ffb7aa775d38c8cf63cdb990003ebf5d.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
-const webpack = require("webpack");
-const path = require("path");
-const process = require("process");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
-const isProd = process.env.NODE_ENV === "production";
-const ifProd = (x) => isProd && x;
-const removeEmpty = (arr) => arr.filter(Boolean);
-
 module.exports = {
   entry: "./src/main.ts",
-  devtool: isProd ? false : "inline-source-map",
+  // devtool: "inline-source-map",
   module: {
     rules: [
       {
@@ -18,16 +9,6 @@ module.exports = {
         exclude: /node_modules/,
       },
     ],
-  },
-  optimization: {
-    noEmitOnErrors: true,
-    minimizer: removeEmpty([
-      ifProd(
-        new UglifyJsPlugin({
-          sourceMap: true,
-        })
-      ),
-    ]),
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
@@ -44,14 +25,4 @@ module.exports = {
     port: 8080,
     hot: true,
   },
-  plugins: [
-    // ... other plugins
-    ifProd(
-      new webpack.SourceMapDevToolPlugin({
-        // this is the url of our local sourcemap server
-        publicPath: "https://localhost:5050/",
-        filename: "[file].map",
-      })
-    ),
-  ],
 };
