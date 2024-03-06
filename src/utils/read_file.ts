25acab0d2c263c24fb5207841341870a47cad7fe
Sanjay Sunil <sanjaysunil@protonmail.com>
2024-03-06 17:41:14 +0000

    push read file fix

--- src/utils/read_file.ts
+++ src/utils/read_file.ts
@@ -5,7 +5,7 @@
  */
 export function read_file(event: Event): Promise<any> {
   // Return a promise to read the file.
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     // Get the target of the event.
     const target = event.target as HTMLInputElement;
     // Get the file from the target.
@@ -18,10 +18,15 @@ export function read_file(event: Event): Promise<any> {
     reader.onload = (event: Event): any => {
       // Get the result of the file reader as a string.
       const result = (event.target as FileReader).result as string;
-      // Parse the result as JSON.
-      const data = JSON.parse(result);
-      // Return the data from the file.
-      resolve(data);
+      if (file.name.split(".").pop() !== "json") {
+        // Reject the promise if the file is not a JSON file.
+        reject(alert("Please upload a JSON file."));
+      } else {
+        // Parse the result as JSON.
+        const data = JSON.parse(result);
+        // Return the data from the file.
+        resolve(data);
+      }
     };
   });
 }
